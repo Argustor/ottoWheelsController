28196846f88c28354e6d7945d389c4b31f8f2815
Argustor <renatoplatz11@outlook.com>
2023-04-03 15:32:29 -0300

    work in progress

---
+++ ottoWheels.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC05(10, 11);
+
+Servo meuServo1;
+Servo meuServo2;
+
+void setup(){
+Serial.begin(9600);
+HC05.begin(9600);
+meuServo1.attach(2);
+meuServo2.attach(3);
+}
+
+void loop(){
+  
+
+  switch (readNluetooh {
+
+    case 'a':
+    for (int i = 1500; i < 2500; i--) {
+    meuServo1.writeMicroseconds(i);
+    meuServo2.writeMicroseconds(4000 - i);
+    delay(10);
+  }
+  }
+
+  }
