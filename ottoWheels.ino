41e2f7ef818fbe3807ae741735a3018c240e12d5
Argustor <renatoplatz11@outlook.com>
2023-04-04 14:18:29 -0300

    new update

--- ottoWheels.ino
+++ ottoWheels.ino
@@ -1,10 +1,15 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial HC05(10, 11);
+SoftwareSerial HC05(11, 12);
 
 Servo meuServo1;
 Servo meuServo2;
+
+int state = 0;
+int state2 = 0;
+int state3 = 0;
+
 
 void setup(){
 Serial.begin(9600);
@@ -14,22 +19,36 @@ meuServo2.attach(3);
 }
 
 void loop(){
+  if (HC05.available() > 0) {
+    char data = HC05.read();
+    executeAction(data);
+  }
+}
+
+void executeAction(char action) {
+  switch (action) {
+    case 'f':
+      moveForward();
+      break;
+    case 'b':
+      moveBackward();
+      break;
+  }
+}
+
+void moveForward() {
+  for (int i = 1500; i < 2500; i--) {
+    meuServo1.writeMicroseconds(i);
+    meuServo2.writeMicroseconds(4000 - i);
+    delay(10);
+  }
   
+}
 
-  switch (readNluetooh {
-
-    case 'forward':
-    for (int i = 1500; i < 2500; i--) {
+void moveBackward(){
+  for (int i = 2500; i > 1500; i++) {
     meuServo1.writeMicroseconds(i);
     meuServo2.writeMicroseconds(4000 - i);
     delay(10);
-    break;
   }
-    case 'backward':
-    for (int i = 2500; i > 1500; i++) {
-    meuServo1.writeMicroseconds(i);
-    meuServo2.writeMicroseconds(4000 - i);
-    delay(10);
-    break;
-  }
-  }
+}
