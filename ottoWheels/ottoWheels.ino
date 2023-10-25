c04164ef0db1eff0715897f53b86357948b0413c
netorapg <renatoplatz11@outlook.com>
2023-10-25 16:02:22 -0300

    Modificacoes na buzina e na movimentacao foram realizadas, tambem foi adicionado o apk do controle

---
+++ ottoWheels/ottoWheels.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+// Define o pino do servo motor
+int servoPin1 = 6;
+int servoPin2 = 2;
+Servo servo1;
+Servo servo2;
+
+// Define os pinos TX e RX do módulo Bluetooth
+int bluetoothTx = 11;
+int bluetoothRx = 12;
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+int buzzerPin = 3;
+
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  // Verifica se há um sinal recebido do aplicativo via Bluetooth
+  if (bluetooth.available() > 0) {
+    // Lê o sinal recebido do aplicativo
+    char signal = bluetooth.read();
+    
+    if (signal == 'f') {
+      servo1.writeMicroseconds(2000);
+      servo2.writeMicroseconds(1000);
+    }
+    else if (signal == 'b') {
+      servo1.writeMicroseconds(1000);
+      servo2.writeMicroseconds(2000);
+    }
+    else if (signal == 'r') {
+      servo1.writeMicroseconds(2000);
+      servo2.writeMicroseconds(2000);
+    }
+    else if (signal == 'l'){
+      servo1.writeMicroseconds(1000);
+      servo2.writeMicroseconds(1000);
+    }
+    else if (signal == 's') {
+      servo1.writeMicroseconds(1500);
+      servo2.writeMicroseconds(1500);
+    }
+   else if (signal == 'h') {
+      tone(buzzerPin, 2000, 500);
+      delay(500);
+      noTone(buzzerPin);
+    }
+  }
+}
+
+
